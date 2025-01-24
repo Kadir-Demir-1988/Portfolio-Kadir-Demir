bff486c997287bbc5d07a70e3601c8e4fb62e1e5
Kadir-Demir-1988 <163757670+Kadir-Demir-1988@users.noreply.github.com>
2025-01-24 18:05:34 +0100

    768pxfinish

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,12 +13,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class NavbarComponent {
   currentLanguage = 'en';
   isHovered = false;
+  isMenuOpen = false;
 
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use('en');
   }
+
+  
+  toggleMenu = () => (this.isMenuOpen = !this.isMenuOpen);
 
   toggleLanguage(): void {
     this.currentLanguage = this.currentLanguage === 'en' ? 'de' : 'en';
